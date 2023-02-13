81fcb79d7044c588ba1c3e6b936d3f393b902aa8
Monir Hoshen <getmonirr@gmail.com>
2023-02-13 16:27:02 +0600

    login page added

--- script/bank.js
+++ script/bank.js
@@ -1,38 +1,51 @@
 const depositBtn = document.querySelector('#deposit-btn');
 const withdrawBtn = document.querySelector('#withdraw-btn');
+const submitForm = document.querySelector('#submit-form');
+const email = submitForm.querySelector('[type=email]');
+const password = submitForm.querySelector('[type=password]');
+
 
 // handler function 
 
 // deposit handler function
-function depositHandler(){
+function depositHandler() {
     const depositAmount = getInputFieldValueById('deposit-field');
     const previousDeposit = getElementValueById('deposit-amount');
     const currentBalance = getElementValueById('current-balance');
 
     // deposit
-    setInputValueById('deposit-amount',(depositAmount + previousDeposit));
+    setInputValueById('deposit-amount', (depositAmount + previousDeposit));
     // current balance
-    setInputValueById('current-balance',(depositAmount + currentBalance))
+    setInputValueById('current-balance', (depositAmount + currentBalance))
 
 }
 
 // withdraw handler function
-function withdrawHandler () {
+function withdrawHandler() {
     const withdrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdraw = getElementValueById('withdraw-amount');
     const currentBalance = getElementValueById('current-balance');
 
-    if(withdrawAmount > currentBalance){
+    if (withdrawAmount > currentBalance) {
         alert(`insufficient Balance`);
         return;
     }
     // withdraw
-    setInputValueById('withdraw-amount',(withdrawAmount + previousWithdraw));
+    setInputValueById('withdraw-amount', (withdrawAmount + previousWithdraw));
     // current balance
     setInputValueById('current-balance', (currentBalance - withdrawAmount));
 }
 
+// submit handler
+function submitHandler(e) {
+    e.preventDefault();
+    if(!validateEmail(email.value)|| email.value === '' || password.value === '') return;
+    window.location.href = 'bank.html';
+}
 
+
+// for log in
+submitForm.addEventListener('submit', submitHandler);
 // for deposit
 depositBtn.addEventListener('click', depositHandler);
 // for Withdraw
--- script/utilities.js
+++ script/utilities.js
@@ -18,3 +18,11 @@ function setInputValueById (elementId,value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
 }
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
